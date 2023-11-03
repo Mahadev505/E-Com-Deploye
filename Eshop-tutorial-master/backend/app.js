3b2f2bc8e1aeac477b6d2681a5754ba7e686dab3
Mahadev <bammamahadev@gmai.com>
2023-11-03 16:17:33 +0530

    all changes

--- Eshop-tutorial-master/backend/app.js
+++ Eshop-tutorial-master/backend/app.js
@@ -3,14 +3,8 @@ const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 
-app.use(cors({
-  origin: 'https://e-com-deploye-pixt.vercel.app', // Remove the trailing slash
-  credentials: true,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow specific HTTP methods
-  allowedHeaders: "Content-Type,Authorization", // Allow specific headers
-}));
+
 
 app.use(express.json());
 app.use(cookieParser());
