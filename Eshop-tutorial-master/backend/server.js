bf3f1ac27f7f3156a605c0c862775d02c409d736
Mahadev <bammamahadev@gmai.com>
2023-11-03 16:33:24 +0530

    alldone

--- Eshop-tutorial-master/backend/server.js
+++ Eshop-tutorial-master/backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const connectDatabase = require("./db/Database");
 const cloudinary = require("cloudinary");
 
-// Handling uncaught Exception
+
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`shutting down the server for handling uncaught exception`);
