f81c4d87ace9b79b121b2b1931841082b4d838db
Mahadev <bammamahadev@gmai.com>
2023-11-03 17:42:44 +0530

    allowtwocors

--- Eshop-tutorial-master/backend/app.js
+++ Eshop-tutorial-master/backend/app.js
@@ -3,9 +3,12 @@ const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-
-
+app.use(cors({
+  origin: ['https://e-com-deploye-pixt-hglrswlka-mahadevs-projects-7f30d1a2.vercel.app','https://e-com-deploye-pixt.vercel.app','https://e-com-deploye-pixt-git-main-mahadevs-projects-7f30d1a2.vercel.app'],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
