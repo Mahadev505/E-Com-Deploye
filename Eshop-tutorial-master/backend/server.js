cf26c507629cab46b0960ec4187ba9239c74db8e
Mahadev <bammamahadev@gmai.com>
2023-11-03 16:47:51 +0530

    all changes done

--- Eshop-tutorial-master/backend/server.js
+++ Eshop-tutorial-master/backend/server.js
@@ -33,6 +33,7 @@ const server = app.listen(process.env.PORT, () => {
 });
 
 // unhandled promise rejection
+//comment
 process.on("unhandledRejection", (err) => {
   console.log(`Shutting down the server for ${err.message}`);
   console.log(`shutting down the server for unhandle promise rejection`);
